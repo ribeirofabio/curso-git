b8906773066a4547faa75aa52155b445eec681c0
Fabio Rodrigues <fabiorr2011@gmail.com>
2021-04-28 10:35:28 -0300

    Projeto iniciando, mostrando a mensagem Ola mundo

---
+++ S08/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace S08 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ S08_98_Novo_repo_Git_cmd_init/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace S08_98_Novo_repo_Git_cmd_init {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
