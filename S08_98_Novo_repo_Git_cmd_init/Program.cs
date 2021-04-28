73a751c450ffac9a082a62ccbf3b41aa9035c4e5
Fabio Rodrigues <fabiorr2011@gmail.com>
2021-04-28 15:55:48 -0300

    Acrescentada a mensagem desejando um bom dia

--- S08_98_Novo_repo_Git_cmd_init/Program.cs
+++ S08_98_Novo_repo_Git_cmd_init/Program.cs
@@ -4,8 +4,8 @@ namespace S08_98_Novo_repo_Git_cmd_init {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
 
             Console.ReadLine();
         }
