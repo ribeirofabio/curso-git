5d2e5ae2f2114c00a6f307eeed3d3d02c48aeca6
Fabio Rodrigues <fabiorr2011@gmail.com>
2021-04-28 19:00:09 -0300

    Acrescentando tracos

--- S08_98_Novo_repo_Git_cmd_init/Program.cs
+++ S08_98_Novo_repo_Git_cmd_init/Program.cs
@@ -3,10 +3,12 @@
 namespace S08_98_Novo_repo_Git_cmd_init {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("-----------------------");
             Console.WriteLine("Olá mundo");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("-----------------------");
 
             Console.ReadLine();
         }
