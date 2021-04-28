6111ee114911581f73bd127ffd331efb443af0de
Fabio Rodrigues <fabiorr2011@gmail.com>
2021-04-28 10:37:13 -0300

    Acrescentada uma mensagem de Bom dia

--- S08_98_Novo_repo_Git_cmd_init/Program.cs
+++ S08_98_Novo_repo_Git_cmd_init/Program.cs
@@ -4,6 +4,7 @@ namespace S08_98_Novo_repo_Git_cmd_init {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo");
+            Console.WriteLine("Bom dia");
 
             Console.ReadLine();
         }
